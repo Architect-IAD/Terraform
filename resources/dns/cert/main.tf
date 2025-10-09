da1248540a00ce3d089d8de4ecbe3b751a3cf91a
Magnus Reeves <rreeves8@uwo.ca>
2025-10-08 20:13:27 -0400

    save

--- resources/dns/cert/main.tf
+++ resources/dns/cert/main.tf
@@ -22,7 +22,7 @@ variable "domain_validation_options" {
 resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in var.domain_validation_options :
-    dvo.domain_name => {
+    dvo.resource_record_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
       value = dvo.resource_record_value
@@ -34,6 +34,8 @@ resource "aws_route53_record" "cert_validation" {
   type    = each.value.type
   ttl     = 60
   records = [each.value.value]
+  
+  allow_overwrite = true
 }
 
 output "validation_records" {
