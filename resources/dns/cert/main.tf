a445c58c3a2d4b4afb808b9d9476cf0baa610a3b
Magnus Reeves <rreeves8@uwo.ca>
2025-10-10 16:12:49 -0400

    fix

--- resources/dns/cert/main.tf
+++ resources/dns/cert/main.tf
@@ -21,20 +21,21 @@ variable "domain_validation_options" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in var.domain_validation_options :
-    "${dvo.resource_record_name}|${dvo.resource_record_type}" => dvo...
+    for dvo in var.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
 
-  zone_id = data.aws_route53_zone.default.zone_id
-  name    = split("|", each.key)[0]
-  type    = split("|", each.key)[1]
-  ttl     = 60
-
-  # Use the first group's value (they're typically identical)
-  records = [each.value[0].resource_record_value]
-
   allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.default.zone_id
 }
+
 
 output "validation_records" {
   value = [for r in aws_route53_record.cert_validation : r.fqdn]
